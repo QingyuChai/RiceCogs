9aa1f40845cdae38329fa9d2e00bf99d7ff33b3e
QingyuChai <chai.qingyu00@gmail.com>
2017-01-13 13:00:09 +0100

    made the code cleaner

--- cogs/rice.py
+++ cogs/rice.py
@@ -34,8 +34,26 @@ class riceBot:
         """
         Shows bot info about riceBot"""
         channel = ctx.message.channel
-        await self.bot.say('```asciidoc\n= riceBot =\n\nWhat is riceBot? :: \nA friendly Discord bot based on Red that has a lot of handy features. The bot is currently on ' + str(len(self.bot.servers)) + ' Servers and connected to ' + str(len(set(self.bot.get_all_members()))) + ' users.\nHere is a list of basic commands:\n``````md\n< Contact owner = use !contact [message]       >\n< Get help      = use !help or !help [command] >\n```', tts=False)
-        await self.bot.say('```markdown\nTo add the bot to your own server, open this [link](https://discordsites.com/ricebot/)\n```')
+        msg = '```asciidoc\n'
+        msg += '\n\nWhat is riceBot? :: '
+        msg += '\nA friendly Discord bot based on Red that has a lot of handy features.'
+        msg += 'The bot is currently on '
+        msg += str(len(self.bot.servers))
+        msg += ' Servers and connected to '
+        msg += str(len(set(self.bot.get_all_members())))
+        msg += ' users.\nHere is a list of basic commands:'
+        msg += '\n```'
+        msg += '```md\n'
+        msg += '< Contact owner = use !contact [message]       >\n'
+        msg += '< Get help      = use !help or !help [command] >\n'
+        msg += '\n```'
+        await self.bot.say(msg)
+        
+        link = '```markdown\n' 
+        link += 'To add the bot to your own server, '
+        link += 'open this [link](https://discordsites.com/ricebot/)'
+        link += '\n```'
+        await self.bot.say(link)
 
 def setup(bot):
     bot.add_cog(riceBot(bot))
