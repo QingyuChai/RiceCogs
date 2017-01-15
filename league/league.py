ff0f85a5c54e364dd24932213b1bbec7e130c7b7
QingyuChai <chai.qingyu00@gmail.com>
2017-01-15 14:43:15 +0100

    fixed

--- league/league.py
+++ league/league.py
@@ -1,20 +1,20 @@
 import discord
- 
-from __main__ import send_cmd_help 
+
+from __main__ import send_cmd_help
 from discord.ext import commands
 from bs4 import BeautifulSoup
 
-class league:
+class League:
     def __init__(self, bot):
         self.bot = bot
-       
+
     @commands.group(pass_context = True)
     async def opgg(self, ctx):
         """Shows a summoners account on OPGG"""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
             return
-    
+
     @commands.group(pass_context = True)
     async def league(self, ctx):
         """Shows a page on http://leagueoflegends.wikia.com/"""
@@ -39,44 +39,44 @@ class league:
     @opgg.command()
     async def eune(self, *, summoner):
         await self.bot.say("http://eune.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command()
     async def euw(self, *, summoner):
         await self.bot.say("http://euw.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command(aliases = ["kr"])
     async def korea(self, *, summoner):
         await self.bot.say("http://www.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command(aliases = ["jp"])
     async def japan(self, *, summoner):
         await self.bot.say("http://jp.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command(aliases = ["br"])
     async def brazil(self, *, summoner):
         await self.bot.say("http://br.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command(aliases = ["tr"])
     async def turkey(self, *, summoner):
         await self.bot.say("http://tr.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command(aliases = ["oce"])
     async def oceania(self, *, summoner):
         await self.bot.say("http://oce.op.gg/summoner/userName=" + summoner)
-   
+
     @opgg.command()
     async def las(self, *, summoner):
         await self.bot.say("http://las.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command()
     async def lan(self, *, summoner):
         await self.bot.say("http://lan.op.gg/summoner/userName=" + summoner)
-    
+
     @opgg.command(aliases = ["ru"])
     async def russia(self, *, summoner):
         await self.bot.say("http://ru.op.gg/summoner/userName=" + summoner)
 
 
- 
+
 def setup(bot):
-    bot.add_cog(league(bot))
+    bot.add_cog(League(bot))
