20c3bf2f92d019617ee3d218bf4602d10b24ac0c
QingyuChai <chai.qingyu00@gmail.com>
2017-01-15 02:16:11 +0100

    moved count servers and counters to configuration

--- rice/rice.py
+++ rice/rice.py
@@ -2,30 +2,10 @@ import discord
 
 from discord.ext import commands
 
- 
+
 class riceBot:                                                              #change the classname
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def countservers(self):
-        """
-        Checks how many servers the bot is on"""
-        await self.bot.say(len(self.bot.servers))
-    
-    @commands.command()
-    async def serverlist(self):
-        """
-        Checks what servers the bot is on"""
-        await self.bot.say(self.bot.servers)
-        
-
-
-    @commands.command()
-    async def countusers(self):
-        """
-        Checks how many users the bot is connected to"""
-        await self.bot.say(len(set(self.bot.get_all_members())))    
 
     @commands.command(no_pm=True, pass_context=True)
     async def rice(self, ctx):                                              #change the command that starts the function
@@ -46,8 +26,8 @@ class riceBot:                                                              #cha
         msg += '< Get help      = use !help or !help [command] >\n'
         msg += '\n```'
         await self.bot.say(msg)
-        
-        link = '```markdown\n' 
+
+        link = '```markdown\n'
         link += 'To add the bot to your own server, '
         link += 'open this [link](https://discordsites.com/ricebot/)'       #add a link - this should be the bot invite link
         link += '\n```'
