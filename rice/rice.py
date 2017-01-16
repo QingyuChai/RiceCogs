4a58a1b422acf10e82bb59f0e9586826b33d2865
QingyuChai <chai.qingyu00@gmail.com>
2017-01-16 21:55:46 +0100

    upon joining a server the bot sends a message
    for some reason the message is getting double sent

--- rice/rice.py
+++ rice/rice.py
@@ -22,8 +22,8 @@ class riceBot:                                                              #cha
         msg += ' users.\nHere is a list of basic commands:'
         msg += '\n```'
         msg += '```md\n'
-        msg += '< Contact owner = use !contact [message]       >\n'
-        msg += '< Get help      = use !help or !help [command] >\n'
+        msg += '< Contact owner = use rice.contact [message]       >\n'
+        msg += '< Get help      = use rice.help or rice.help [command] >\n'
         msg += '\n```'
         await self.bot.say(msg)
 
@@ -33,5 +33,28 @@ class riceBot:                                                              #cha
         link += '\n```'
         await self.bot.say(link)
 
+
+
+    async def on_server_join(self, server):
+        msg = "```asciidoc\n"
+        msg += "Announcement :: Information\n"
+        msg += "= -=-=-=-=-=-=-=-=-=-=-=- =\n"
+        msg += "Thank you for inviting riceBot!\n"
+        msg += "For basic information on the bot, a list of commands, or to contact the owner, use: \n"
+        msg += "= rice.rice =\n"
+        msg += "= rice.help =\n"
+        msg += "= rice.contact =\n"
+        msg += "To add the bot to your own server, open this:: https://discordsites.com/ricebot/\n"
+        msg += "= -=-=-=-=-=-=-=-=-=-=-=- =\n"
+        msg += "riceBot ~ managed by FwiedWice"
+        msg += "\n```"
+        try:
+            await self.bot.send_message(server, msg)
+        except:
+            pass
+
+
 def setup(bot):
-    bot.add_cog(riceBot(bot))                                               #change the name to what you changed the classname to in line 7
+    n = riceBot(bot)
+    bot.add_listener(n.on_server_join)
+    bot.add_cog(n)                                               #change the name to what you changed the classname to in line 7
