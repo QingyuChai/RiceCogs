4929fc67878a35917dbef6e0b795ff05f08da3fa
QingyuChai <chai.qingyu00@gmail.com>
2017-03-05 00:45:01 +0100

    fixed list_channels

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -15,7 +15,7 @@ class Config:
         self.down_message = "data/servers/down_message.json"
         self.down = dataIO.load_json(self.down_message)
 
-    async def _message_servers(self, ctx, msg):
+    async def _message_servers(self, msg):
         for server in self.bot.servers:
             if server.id in self.riceCog:
                 try:
@@ -95,7 +95,7 @@ class Config:
         # msg += "\n"
         count = 0
         for channel in server.channels:
-            if channel.type != "voice":
+            if channel.type == discord.ChannelType.text:
                 channelname = channel.name.replace("_", "-")
                 msg += "{} :: {}\n".format(channel.id, channelname)
                 count += 1
@@ -164,14 +164,42 @@ class Config:
     @checks.is_owner()
     async def notify(self, ctx, *, content):
         """Notifies every server"""
-        prefix = ctx.prefix
+        if content == "info":
+            msg = "```asciidoc\n"
+            msg += "Announcement :: Information\n"
+            msg += "= -=-=-=-=-=-=-=-=-=-=-=- =\n"
+            msg += "Thank you for inviting riceBot!\n"
+            msg += "For basic information on the bot, a list of commands, or to contact the owner, use: \n"
+            msg += "= rice.rice =\n"
+            msg += "= rice.help =\n"
+            msg += "= rice.contact =\n"
+            msg += "To add the bot to your own server, open this:: https://discordsites.com/ricebot/\n"
+            msg += "= -=-=-=-=-=-=-=-=-=-=-=- =\n"
+            msg += "riceBot ~ managed by FwiedWice"
+            msg += "\n```"
+        else:
+            msg = "```asciidoc\n"
+            msg += "Announcement :: Information\n"
+            msg += content
+            msg += "\n```"
+        await self._message_servers(msg)
 
-        msg = "```asciidoc\n"
-        msg += "Announcement :: Information\n"
-        msg += content
-        msg += "\n```"
+    #def __shutdown(self):
+    #   """Credits to Kowlin for this"""
+    #    self.bot.loop.create_task(self.task())
 
-        await self._message_servers(ctx, msg)
+    #async def task(self):
+    #    msg = ("```asciidoc\n"
+    #           "Announcement :: Shutdown\n"
+    #           "riceBot shutting down... Will be up again soon!"
+    #           "\n```")
+    #    for server in self.bot.servers:
+    #        try:
+    #            await self.bot.send_message(server, msg)
+    #        except:
+    #            pass
+    #    await self.bot.say("asdasd")
+
 
     @commands.command(pass_context=True)
     @checks.is_owner()
@@ -180,7 +208,7 @@ class Config:
         if msg == None:
             msg = ("```asciidoc\n"
                    "Announcement :: Shutdown\n"
-                   "Bot shutting down... Will be up again soon!"
+                   "riceBot shutting down... Will be up again soon!"
                    "\n```")
         await self.bot.say("The shutdown message is: ")
         await self.bot.say(msg)
@@ -191,15 +219,15 @@ class Config:
     @commands.command(pass_context=True)
     @checks.is_owner()
     async def shutdown(self, ctx, silently : bool=False):
-        """Shuts down the bot"""
+        """Shuts down riceBot"""
         if "Message" in self.down:
             msg = self.down["Message"]
         else:
             msg = ("```asciidoc\n"
                    "Announcement :: Shutdown\n"
-                   "Bot shutting down... Will be up again soon!"
+                   "riceBot shutting down... Will be up again soon!"
                    "\n```")
-        await self._message_servers(ctx, msg)
+        await self._message_servers(msg)
         try: # We don't want missing perms to stop our shutdown
             if not silently:
                 await self.bot.say("Shutting down... ")
